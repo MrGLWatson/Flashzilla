1126ff579282ed3ec2fd864e1761acad8b1b2184
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-08-07 10:59:19 +0100

    SwiftUI app moves to the background / foreground

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,23 +5,15 @@
 //  Created by Gary Watson on 06/08/2020.
 //  Copyright © 2020 Gary Watson. All rights reserved.
 //
-// triggering events repeatedly using a timer
+// SwiftUI app moves to the background / foreground
 
 import SwiftUI
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
-    
     var body: some View {
         Text("Hello")
-            .onReceive(timer) { time in
-                if self.counter == 5 {
-                    self.timer.upstream.connect().cancel()
-                } else {
-                    print("The time is now \(time)")
-                }
-                self.counter += 1
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                print("Moving back to the foreground")
         }
     }
 }
