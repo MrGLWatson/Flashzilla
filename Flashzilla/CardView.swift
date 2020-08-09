2d10453db3b1f117f28f05ca63e423876345b156
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-08-09 12:01:06 +0100

    Coloring views, add count down, ending the app with allowsHitTesting

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     let card: Card
     var removal: (() -> Void)? = nil
     
@@ -18,7 +19,14 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.white)
+                .fill(
+                    differentiateWithoutColor ? Color.white : Color.white
+                .opacity(1 - Double(abs(offset.width / 50))))
+                .background(
+                    differentiateWithoutColor ? nil :
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(offset.width > 0 ? Color.green : Color.red)
+            )
                 .shadow(radius: 10)
             VStack {
                 Text(card.prompt)
@@ -37,8 +45,8 @@ struct CardView: View {
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .offset(x: offset.width * 5, y: 0)
         .opacity(2 - Double(abs(offset.width / 50)))
-    .gesture(
-        DragGesture()
+        .gesture(
+            DragGesture()
             .onChanged { gesture in
                 self.offset = gesture.translation
     }
